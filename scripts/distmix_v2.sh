73d131d11a39269860b35ec3d75bb287234c9816
yagoubali <yagoubali@gmail.com>
2022-02-10 09:43:05 -0300

    Users should provide z score

--- scripts/distmix_v2.sh
+++ scripts/distmix_v2.sh
@@ -93,27 +93,6 @@ fi
 
 
 
-z=$(echo $line |tr " " "\n"|grep -i 'z'| cut  -f1);      # Zscore column
-
-###### estimate z score if it is not exist
-if [[ $z -eq "" ]];
-then
-  Rscript --vanilla ${bin_dir}/z_estimates.R ${input_dir}/$gwas_summary
-fi
-
-
-##### check again for zcore 
-
-read -r line < "$gwas_summary"     ## read first line from file into 'line'
-z=$(echo $line |tr " " "\n"|grep -i 'z'| cut -f1);
-
-if [[ $z -eq "" ]];
-then
-  echo "please provide:  z score, or beta and standard error, or beta and pvalue";
-  exit 1;
-fi
-
-
 ##### Imputation
 ### 4 senarios 
 ###  ->1 User provided af  (cohort reference allele frequency (RAF)) && provided chromosome
@@ -124,16 +103,16 @@ fi
 cmd=''
 if [[ $af != "" ]] && [[ $chr != "" ]]; then
        cmd="-c $chr "
-elif [[ $af != "" ]] && [[ -z "$chr" ]]; then 
+elif [[ $af != "" ]] && [[ $chr = 'all' ]]; then 
        cmd=''
 elif [[ -z "$af" ]] && [[ $chr != "" ]]; then 
        cmd="-w $Populations_Weight -c $chr "
-elif [[ -z "$af" ]] && [[ -z "$chr" ]]; then 
+elif [[ -z "$af" ]] && [[ $chr = 'all' ]]; then 
      cmd="-w $Populations_Weight  "
 fi    
 
 
-#./distmix_v2.sh sample.input.chr22.txt output 2 false 0.6 0.2 0.1 0.1 0.0 22
+#./distmix_v2.sh sample.input.chr22.txt output 2 false 0.6 0.2 0.1 0.1 0.0 22 
 #./distmix_v2.sh sample.input.chr22.wthAf1.txt 2 output true 22
 #./distmix_v2.sh sample.input.chr22.txt output 1 false 0.6 0.2 0.1 0.1 0.0 22
 #./distmix_v2.sh sample.input.chr22.wthAf1.txt output 1 true 22
